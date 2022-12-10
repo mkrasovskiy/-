6229ca4dfe7149fffc7c23883e5ae1b88b0e89c0
Maks <kpasowsky@gmail.com>
2022-12-10 23:15:56 +0300

    Example013

---
+++ Example013/Program.cs
@@ -0,0 +1,21 @@
+﻿string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
++ "ежелт бы вас послали вместо нашего милого Винценгероде, "
++ "вы бы взяли приступомсогласие прусского короляю. "
++ "Вы так красноречивы. Вы дадите мне чаю? ";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
