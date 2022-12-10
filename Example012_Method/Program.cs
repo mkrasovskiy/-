04de24fa0dad51d6a95fbad42b22bf56cb1da9d9
Maks <kpasowsky@gmail.com>
2022-12-10 22:49:49 +0300

    Цикл в цикле

--- Example012_Method/Program.cs
+++ Example012_Method/Program.cs
@@ -59,3 +59,13 @@ string Method4(int count, string text)
 }
 string res = Method4(10, "z");
 Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++ )
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
